1cc4c885a1efb71e2251a78c6b9c7d95e08f6cd5
ciec20151104682 <s20151104682@mac21.local>
2018-05-11 17:22:04 +0800

      项目小改
    1.运用for语句，将按钮插入后进行页面布局，并完成点击事件
    2.设置二维数组
    问题：
    unrecognized selector
    疑似连接两个控件或未连接成功

--- Game/Game/ViewController.swift
+++ Game/Game/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var allButtens = [[UIButton]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -16,8 +18,30 @@ class ViewController: UIViewController {
         backgroundImageview.frame =  CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 320, height: 568))
         backgroundImageview.image = UIImage(named:"background.jpg")
         self.view.addSubview(backgroundImageview)
+        produceAllButtons()
     }
-
+    func produceAllButtons(){
+        for i in 1..<10{
+             var rowButtons = [UIButton]()
+            for j in 1..<10{
+                let btn = UIButton()
+                rowButtons.append(btn)
+                btn.addTarget(self,action:Selector(("clickme")), for: UIControlEvents.touchUpInside)
+                if i%2==0 {
+                    btn.frame =  CGRect(x: (20+24*j),y :(170+24*i), width: 24, height: 24)
+                }else{
+                    btn.frame =  CGRect(x: (32+24*j),y :(170+24*i), width: 24, height: 24)
+                }
+                self.view.addSubview(btn)
+                btn.setImage(UIImage(named:"gray.png"), for: UIControlState())
+            }
+            allButtens.append(rowButtons)
+        }
+    }
+    func clickme(btn: UIButton) {
+        btn.setImage(UIImage(named:"yellow.png"), for: UIControlState())
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
