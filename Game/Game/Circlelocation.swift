70e33f1e59090ee68c04f4a3db420c82dbceeb8f
ciec20151104682 <s20151104682@mac21.local>
2018-07-04 20:33:44 +0800

    基本完成项目
    实现以最大通路或最短路径相结合的方式移动兔子
    在游戏结束时弹出对话框

--- Game/Game/Circlelocation.swift
+++ Game/Game/Circlelocation.swift
@@ -1,43 +1,115 @@
-//
-//  Circlelocation.swift
-//  Game
-//
-//  Created by s20151104682 on 18/6/26.
-//  Copyright © 2018年 s20151104682. All rights reserved.
-//
-
 import Foundation
 class CircleLocation{
     var row : Int
     var col : Int
     var cost : Int = -100
+    var path : Int = -100
     init(row:Int,col:Int) {
         self.col=col
         self.row=row
     }
+    func isBoundary()->Bool{
+        if (row==0||col==0||row==8||col==8){
+            return true
+        }
+        return false
+    }
+    func calculateCost() -> Int {
+        if map[row][col]==1{
+            cost=100;
+            return cost
+        }
+        if isBoundary(){
+            cost=0
+            return cost
+        }
+        let allConnects=self.getAllConnectLocation()
+        cost=allConnects.count
+        return cost
+    }
+    func calculatePath() -> Int {
+        if map[row][col]==1{
+            path=100;
+            return path
+        }
+        if isBoundary(){
+            path=0
+            return path
+        }
+        let allConnects=self.getAllConnectLocation()
+        var min = 100
+        for i in 0 ..< allConnects.count {
+            var tmp = allConnects[i].path
+            if tmp<0{
+                tmp = -tmp
+            }
+            if min>tmp{
+                min=tmp
+            }
+        }
+        path=min+1
+        return path
+    }
+    func isInCircle() -> Int {
+        var allConnects = self.getAllLocation()
+        var num = 0
+        for i in 0 ..< allConnects.count{
+            if allConnects[i].path>=100{
+                num=num+1
+            }
+        }
+        if num==6{
+            return 1
+        }else{
+            return 0
+        }
+    }
+    func isLessThan(loc:CircleLocation) -> Bool {
+        if cost < loc.cost {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    func isMoreThan(loc:CircleLocation) -> Bool {
+        if path > loc.path {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    func compare(loc: CircleLocation) -> Bool{
+        if hasCircle == 0{
+            return isMoreThan(loc: loc)
+        }else{
+            return isLessThan(loc: loc)
+        }
+    }
     func getLeft() -> CircleLocation? {
         var newp : CircleLocation? = nil
         if col>0{
-            newp=allCircleLocations[row][col-1]
+            newp=allCircleLocation[row][col-1]
         }
         return newp
     }
     func getRight() -> CircleLocation? {
         var newp : CircleLocation? = nil
         if (col<8){
-            newp=allCircleLocations[row][col+1]
+            newp=allCircleLocation[row][col+1]
         }
         return newp
     }
     func getLeftUp() -> CircleLocation? {
         var newp : CircleLocation? = nil
         if (row<8) {
-            let p=allCircleLocations[row-1][col]
+            let p=allCircleLocation[row-1][col]
             if (row%2==0) {
                 if(col==0){
                     newp=nil
                 }else{
-                    newp=allCircleLocations[row-1][col-1]
+                    newp=allCircleLocation[row-1][col-1]
                 }
             }else{
                 newp=p
@@ -48,7 +120,7 @@ class CircleLocation{
     func getRightUp() -> CircleLocation? {
         var newp : CircleLocation? = nil
         if (row<8) {
-            let p=allCircleLocations[row-1][col]
+            let p=allCircleLocation[row-1][col]
             if (row%2==0) {
                 newp=p
             }else{
@@ -56,7 +128,7 @@ class CircleLocation{
                 if(col==8){
                     newp=nil
                 }else{
-                    newp=allCircleLocations[row-1][col+1]
+                    newp=allCircleLocation[row-1][col+1]
                 }
             }
         }
@@ -65,13 +137,13 @@ class CircleLocation{
     func getLeftDown() -> CircleLocation? {
         var newp : CircleLocation? = nil
         if (row < 8) {
-            let p = allCircleLocations[row+1][col]
+            let p = allCircleLocation[row+1][col]
             if (row % 2 == 0) {
                 if (col == 0) {
                     newp = nil
                 }
                 else {
-                    newp = allCircleLocations[row+1][col-1]
+                    newp = allCircleLocation[row+1][col-1]
                 }
             }
             else {
@@ -83,7 +155,7 @@ class CircleLocation{
     func getRightDown() -> CircleLocation? {
         var newp : CircleLocation? = nil
         if (row < 8) {
-            let p = allCircleLocations[row+1][col]
+            let p = allCircleLocation[row+1][col]
             if (row % 2 == 0) {
                 newp = p
             }
@@ -92,7 +164,7 @@ class CircleLocation{
                     newp = nil
                 }
                 else {
-                    newp = allCircleLocations[row+1][col+1]
+                    newp = allCircleLocation[row+1][col+1]
                 }
             }
         }
@@ -138,5 +210,32 @@ class CircleLocation{
         }
         return arr
     }
-
+    func getAllLocation() -> [CircleLocation] {
+        var arr = [CircleLocation]()
+        var cl = getLeftUp()
+        if let temp = cl {
+            arr.append(temp)
+        }
+        cl = getLeft()
+        if let temp = cl {
+            arr.append(temp)
+        }
+        cl = getLeftDown()
+        if let temp = cl {
+            arr.append(temp)
+        }
+        cl = getRightDown()
+        if let temp = cl {
+            arr.append(temp)
+        }
+        cl = getRight()
+        if let temp = cl {
+            arr.append(temp)
+        }
+        cl = getRightUp()
+        if let temp = cl {
+            arr.append(temp)
+        }
+        return arr
+    }
 }
--- Game/Game/ViewController.swift
+++ Game/Game/ViewController.swift
@@ -1,12 +1,6 @@
-//
-//  ViewController.swift
-//  Game
-//
-//  Created by s20151104682 on 18/5/9.
-//  Copyright © 2018年 s20151104682. All rights reserved.
-//
-
+import Foundation
 import UIKit
+var hasCircle = 0
 var map=[[0,0,0,0,0,0,0,0,0],
          [0,0,0,0,0,0,0,0,0],
          [0,0,0,0,0,0,0,0,0],
@@ -16,27 +10,26 @@ var map=[[0,0,0,0,0,0,0,0,0],
          [0,0,0,0,0,0,0,0,0],
          [0,0,0,0,0,0,0,0,0],
          [0,0,0,0,0,0,0,0,0]]
-var allCircleLocations = [[CircleLocation]]()
+var allCircleLocation = [[CircleLocation]]()
 func produceAllCircleLocations(){
-    for i in 1..<10{
+    for i in 0..<9{
         var rowCircleLocations = [CircleLocation]()
-        for j in 1..<10{
+        for j in 0..<9{
             let loc = CircleLocation(row:i,col:j)
             rowCircleLocations.append(loc)
         }
-        allCircleLocations.append(rowCircleLocations)
+        allCircleLocation.append(rowCircleLocations)
     }
 }
-
-
 class ViewController: UIViewController {
     var allButtens = [[UIButton]]()
-    var rib = CircleLocation(row:4,col:4)
+    var rib = CircleLocation(row: 4,col: 4)
     let ribImageview = UIImageView()
-    
+    var pathNumber  = 0
+    var isGameOver = 0
+    var clickedPoint:CircleLocation?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         let backgroundImageview = UIImageView()
         backgroundImageview.frame =  CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 320, height: 568))
         backgroundImageview.image = UIImage(named:"background.jpg")
@@ -45,6 +38,21 @@ class ViewController: UIViewController {
         produceRib()
         produceGameLevel()
         produceAllCircleLocations()
+    }
+    func initilizeGame(){
+        for i in 0..<9{
+            for j in 0..<9{
+                allButtens[i][j].setImage(UIImage(named: "gray.png"),for:UIControlState.normal)
+                map[i][j] = 0
+            }
+        }
+        rib.row = 4
+        rib.col = 4
+        map[4][4]=1
+        ribImageview.frame = CGRect(x: (20+24*4),y :(170+24*3), width: 24, height: 48)
+        produceGameLevel()
+        pathNumber  = 0
+        isGameOver = 0
     }
     func produceGameLevel(){
         let gamelevel = (Int)(arc4random()%40+10)
@@ -68,12 +76,13 @@ class ViewController: UIViewController {
         ribImageview.animationImages=[leftImage!,middleImage!,rightImage!]
         ribImageview.animationDuration=1.0
         ribImageview.startAnimating()
+        map[4][4]=1
         self.view.addSubview(ribImageview)
     }
     func produceAllButtons(){
-        for i in 1..<10{
+        for i in 0..<9{
              var rowButtons = [UIButton]()
-            for j in 1..<10{
+            for j in 0..<9{
                 let btn = UIButton()
                 rowButtons.append(btn)
                 btn.addTarget(self,action:#selector(clickMe(btn:)), for: UIControlEvents.touchUpInside)
@@ -88,40 +97,163 @@ class ViewController: UIViewController {
             allButtens.append(rowButtons)
         }
     }
+    func getButtonRow(btn:UIButton) -> Int {
+        let y = (Int)(btn.frame.origin.y)
+        let row = (y - 170) / 24
+        return row
+    }
+    func getButtonCol(btn:UIButton) -> Int {
+        let x = (Int)(btn.frame.origin.x)
+        let y = (Int)(btn.frame.origin.y)
+        let row = (y - 170) / 24
+        var col = 0
+        if (row % 2 == 0) {
+            col = (x - 20) / 24
+        }
+        else {
+            col = (x - 32) / 24
+        }
+        return col
+    }
     func clickMe(btn: UIButton) {
         btn.setImage(UIImage(named:"yellow.png"), for: UIControlState())
-        ribAutoGo()
-        
+        let row = getButtonRow(btn: btn)
+        let col = getButtonCol(btn: btn)
+        clickedPoint=allCircleLocation[row][col]
+        updateCost(row: row,col:col)
+        pathNumber=pathNumber+1
+        if (self.isGameOver == 1 && row == rib.row && col == rib.col) {
+            showWinAlertView()
+            return
+        }
+        else if (self.isGameOver == 1) {
+        }
+        else {
+            isGameOver = ribAutoGo()
+            if isGameOver == -1 {
+                showLoseAlertView()
+                return
+            }
+            else if (self.isGameOver == 1 && row == rib.row && col == rib.col) {
+                showWinAlertView()
+                return
+            }
+        }
+    }
+    func runAgain(){
+        initilizeGame()
+    }
+    func showLoseAlertView() {
+        let alert = UIAlertController(title: "抱歉，兔子跑掉了！", message: "你失败了！加油！", preferredStyle: .alert)
+        let actionYes = UIAlertAction(title: "退出游戏？", style: .default, handler: {act in exit(-1)})
+        let actionNo = UIAlertAction(title: "再来一次？", style: .default, handler: {act in self.runAgain()})
+        alert.addAction(actionYes)
+        alert.addAction(actionNo)
+        self.present(alert, animated: true, completion: nil)
+    }
+    func showWinAlertView() {
+        let alert = UIAlertController(title: "你的步数是：\(pathNumber)次", message: "你成功抓住兔子了！", preferredStyle: .alert)
+        let actionYes = UIAlertAction(title: "退出游戏？", style: .default, handler: {act in exit(-1)})
+        let actionNo = UIAlertAction(title: "再来一次？", style: .default, handler: {act in self.runAgain()})
+        alert.addAction(actionYes)
+        alert.addAction(actionNo)
+        self.present(alert, animated: true, completion: nil)
+    }
+    func updateCost(row:Int,col:Int)
+    {
+        map[row][col]=1
+        calAllCost()
     }
     func getBestLocation()->CircleLocation?{
         let ribAllConnect = rib.getAllConnectLocation()
         if ribAllConnect.count>0{
-            return ribAllConnect[0]
+            var best = ribAllConnect[0]
+            if best.isBoundary(){
+                return best
+            }
+            for i in 1 ..< ribAllConnect.count {
+                if ribAllConnect[i].isBoundary(){
+                    best=ribAllConnect[i]
+                    break
+                }
+                if best.compare(loc: ribAllConnect[i]){
+                    best=ribAllConnect[i]
+                }
+            }
+            return best
         }else{
             return nil
         }
-        
     }
-    func ribAutoGo() {
+    func ribAutoGo() -> Int {
         let temp = getBestLocation()
         if let best=temp {
+            if (clickedPoint?.row == rib.row && clickedPoint?.col == rib.col) {
+            }
+            else {
+                map[rib.row][rib.col]=0
+            }
             rib.row=best.row
             rib.col=best.col
             let i = best.row
             let j = best.col
-            if i%2==0 {
+            map[i][j]=1
+            if i % 2 == 0 {
                 ribImageview.frame = CGRect(x: (20+24*j),y :(170+24*(i-1)), width: 24, height: 48)
             }else{
                 ribImageview.frame = CGRect(x: (32+24*j),y :(170+24*(i-1)), width: 24, height: 48)
             }
+            if rib.isBoundary(){
+                return -1
+            }
         }
-        
+        else{
+            return 1
+        }
+        return 0
+    }
+    func clearAllCost() {
+        for i in 0..<9{
+            for j in 0..<9{
+                allCircleLocation[i][j].cost = -100;
+                allCircleLocation[i][j].path = -100;
+            }
+        }
+    }
+    func calAllCost() {
+        clearAllCost()
+        for i in 0..<9{
+            for j in 0..<9{
+                allCircleLocation[i][j].calculateCost()
+            }
+        }
+        for i in 0..<9{
+            for j in 0..<9{
+                allCircleLocation[i][j].calculatePath()
+                allCircleLocation[j][i].calculatePath()
+            }
+        }
+        for i in 0..<9{
+            for j in 0..<9{
+                allCircleLocation[i][8-j].calculatePath()
+                allCircleLocation[j][8-i].calculatePath()
+            }
+        }
+        for i in 0..<9{
+            for j in 0..<9{
+                allCircleLocation[8-i][j].calculatePath()
+                allCircleLocation[8-j][i].calculatePath()
+            }
+        }
+        for i in 0..<9{
+            for j in 0..<9{
+                allCircleLocation[8-i][8-j].calculatePath()
+                allCircleLocation[8-j][8-i].calculatePath()
+            }
+        }
+        hasCircle = rib.isInCircle()
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
